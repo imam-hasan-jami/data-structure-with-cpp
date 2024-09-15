95f0e4714270364ffc53b7712a04225a1e8fcd93
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2024-09-15 09:04:24 +0600

    updated pattern1
    using function and take user input

--- Patterns/pattern1.cpp
+++ Patterns/pattern1.cpp
@@ -1,19 +1,33 @@
 #include<iostream>
 using namespace std;
 
-int main() {
-    
-    for(int i=0; i<4; i++) {
-        for(int j=0; j<4; j++) {
+void printPattern(int n) {
+    for(int i=0; i<n; i++) {
+        for(int i=0; i<n; i++) {
             cout << "* ";
         }
         cout << endl;
     }
+}
+
+int main() {
+    
+    // for(int i=0; i<4; i++) {
+    //     for(int j=0; j<4; j++) {
+    //         cout << "* ";
+    //     }
+    //     cout << endl;
+    // }
+
+    int n;
+    cout << "Enter the number of rows: ";
+    cin >> n;
+    printPattern(n);
     
     return 0;
 }
 
-//output :
+//output when n=4 :
 // * * * * 
 // * * * * 
 // * * * * 
